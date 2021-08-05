0212587c023ddd557fe6f18eae89e5dc0bf2da7e
shabeeb66 <88030083+shabeeb66@users.noreply.github.com>
2021-08-05 22:50:25 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔙 Back to Group', url="https://t.me/astor_movies"
                                 )
                         ]
                     ]
