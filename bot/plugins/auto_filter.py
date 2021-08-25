50557b43c34028bd2ec018c3b2922a9d23f41269
shabeeb66 <88030083+shabeeb66@users.noreply.github.com>
2021-08-26 00:25:41 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found Results{(len_results)} \n \n Your Request : <code>{query}</code>",
+                text=f"✦ Found Results : {(len_results)} \n \n ✦ Your Request : <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
